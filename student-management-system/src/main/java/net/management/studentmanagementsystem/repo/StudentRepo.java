1f58587166687a5a3d73a1c3931ef4c02bcd3cef
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-02-04 01:25:29 +0545

    extends JpaRepo

--- student-management-system/src/main/java/net/management/studentmanagementsystem/repo/StudentRepo.java
+++ student-management-system/src/main/java/net/management/studentmanagementsystem/repo/StudentRepo.java
@@ -10,5 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface StudentRepo extends JpaRepository<Student, Long> {
 //    Also JPaRepo provides auto Transitional to all method.
 
-    
+
 }
